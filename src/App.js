ffe0f9b7871db7236403ac5d9e4e6f8b2102d6bd
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-24 12:30:48 -0500

    added a state to m manage the search inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios"
+
 import SearchBar from "./components/SearchBar"
 import Nominations from "./components/Nominations"
 
@@ -8,13 +10,25 @@ import Nominations from "./components/Nominations"
 
 export default function App(props) {
 
+  //tracking the searchbar state
+
+  const [term, setTerm] = useState("")
+
+  useEffect(() => {
+    const apiKey = '3b233bc1'
+    const url = `http://omdbapi.com/?apikey=${apiKey}&s=${term}`
+    const response = axios.get(url).then(res => console.log(res.data))
+   
+  }, [term])
+
+
   return (
     <Fragment>
       <header className="logo">
         <img src={process.env.PUBLIC_URL + '/movies.png'} alt="Brand" />
       </header>
       <main>
-        <SearchBar/>
+        <SearchBar onChange={(event)=>setTerm(event.target.value)} />
         <Nominations />
       </main>
     </Fragment>
