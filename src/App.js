6e31fa2a353adcce4fcb1c8930f1a3c3bc590f33
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-24 23:56:21 -0500

    added the searches component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React, { Fragment, useEffect, useState } from "react";
-import axios from "axios"
+import React from "react";
 
-import SearchBar from "./components/SearchBar"
-import Nominations from "./components/Nominations"
-
-//import axios from 'axios'
-
-
+import Searches from './searches'
 
 export default function App(props) {
-
-  //tracking the searchbar state
-
-  const [term, setTerm] = useState("")
-
-  useEffect(() => {
-    const apiKey = '3b233bc1'
-    const url = `http://omdbapi.com/?apikey=${apiKey}&s=${term}`
-    const response = axios.get(url).then(res => console.log(res.data))
-   
-  }, [term])
-
-
   return (
-    <Fragment>
-      <header className="logo">
-        <img src={process.env.PUBLIC_URL + '/movies.png'} alt="Brand" />
-      </header>
-      <main>
-        <SearchBar onChange={(event)=>setTerm(event.target.value)} />
-        <Nominations />
-      </main>
-    </Fragment>
-  );
+    <Searches />
+  )
 }
 
