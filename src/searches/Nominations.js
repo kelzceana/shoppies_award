c418c55f66e5d3d8a6b9e3f796f8325630a0b620
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-24 23:53:57 -0500

    added the movie component and its propd

--- src/searches/Nominations.js
+++ src/searches/Nominations.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Movie from './Movie'
 
 export default function Nominations(props) {
+  console.log(props, "nominations")
   return (
     <section className="nominations">
       <h1>Your Nominations</h1>
-      <div className="nomination-bar"></div>
+      <Movie movie={props.movie}/>
     </section>
   );
 }
