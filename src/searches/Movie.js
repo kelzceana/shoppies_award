b6e944dbed3d9c1021bcf22fb70b41f07298e9cd
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-24 23:55:19 -0500

    modified the movie component

--- src/searches/Movie.js
+++ src/searches/Movie.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default function Movie(props) {
-  console.log(props.movie
-    , "movie component")
+ 
   return (
     <article className="movie">
       <img className="movie__thumbnail" src ={props.movie.Poster} alt="movie" />
       <div className="movie__info">
         <div className="movie__name">{props.movie.Title}</div>
-        <div className="movie__artist"></div>
+        <div className="movie__artist">{props.movie.Year}</div>
       </div>
+      <button className="nominate-btn" onClick={props.onClick}>Nominate</button>
     </article>
   );
 }
