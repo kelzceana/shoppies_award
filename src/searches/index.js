a13a78d3862f86b83555afc5d1fc66d3375200bb
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-25 21:24:30 -0500

    added condition that prevent the nomination componenet from renderiing if no nomination

--- src/searches/index.js
+++ src/searches/index.js
@@ -10,40 +10,37 @@ import Nominations from "./Nominations"
 const Searches = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
-  const [nominate, setNominate] = useState([]);
+  const [lists, setLists] = useState([]);
   
 
   useEffect(() => {
-    const apiKey = '3b233bc1'
-    const url = `http://omdbapi.com/?apikey=${apiKey}&s=${term}`
-     axios.get(url).then(res => {
-       setResults(res.data.Search)
-     })
+    
+      const apiKey = '3b233bc1'
+      const url = `http://omdbapi.com/?apikey=${apiKey}&s=${term}`
+      axios.get(url).then(res => {
+        setResults(res.data.Search)
+      })
   }, [term])
 
-  // const onSubmit = () => {
-  //   console.log('ond')
-    
-  // }
-
+  const nominateMovie = (movie) => {
+    console.log(lists, "nominateMovie")
+    const newList = lists.concat(movie)
+    setLists(newList)
+  }
+  console.log(lists, "hey list")
   return (
     <>
       <header className="logo">
         <img src={process.env.PUBLIC_URL + '/movies.png'} alt="Brand" />
       </header>
       <main>
-        <>
+      
         <SearchBar onChange={(e)=>setTerm(e.target.value)} />
-        </>
          {results && <Results 
             results={results}
-            onClick = {setNominate}
+            onClick = {nominateMovie}
             />}
-
-         <Nominations    
-          movie ={nominate}
-        />
-       
+          {lists.length > 0 && <Nominations movie ={lists}/>}
       </main>
     </>
   ); 
