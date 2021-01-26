f2ad4a8623598ce6803155806e0194f1b157c187
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-25 21:26:22 -0500

    refactored to dynamically render the movie component using a map function

--- src/searches/Nominations.js
+++ src/searches/Nominations.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Movie from './Movie'
 
 export default function Nominations(props) {
+    const movies = props.movie
   console.log(props, "nominations")
   return (
     <section className="nominations">
       <h1>Your Nominations</h1>
-      <Movie movie={props.movie}/>
+      {movies.map(movie => <Movie movie={movie} message="Remove" />)}
+      {console.log(props.movie, 'kkkkkk')}
     </section>
   );
 }
