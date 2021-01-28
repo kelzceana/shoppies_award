f63eb5630ba26fd7f9576181cc6f5e850e00429f
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-27 22:17:00 -0500

    added a handleonchange function for the searchbar component

--- src/searches/index.js
+++ src/searches/index.js
@@ -40,14 +40,20 @@ const Searches = () => {
     const newList = lists.filter(item => item !== movie)
     setLists(newList)
   }
+
+  const handleOnChange = (e) => {
+    setTerm(e.target.value)
+    if (e.target.value === "") setError("")
+  }
+
   return (
     <>
       <header className="logo">
         <img src={process.env.PUBLIC_URL + '/movies.png'} alt="Brand" />
       </header>
       <main>
-        <SearchBar onChange={(e)=>setTerm(e.target.value)} />
-        {!results && <Error  message={error}/>}
+        <SearchBar onChange={(e)=>handleOnChange(e)} />
+        {!results && error !== "Incorrect IMDb ID." && <Error  message={error}/>}
         {results && <Results results={results} onClick = {nominateMovie}/>}
         {lists.length > 0 && <Nominations movie ={lists} onClick = {remove}/>}
       </main>
