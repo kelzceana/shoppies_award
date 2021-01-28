e00609401a2f4f7ca15e7fe0920fa55b42677a25
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-27 21:20:29 -0500

    modified the onclick event

--- src/searches/Movie.js
+++ src/searches/Movie.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Movie(props) {
- 
+  const [isDisabled, setDisabled] = useState(false)
+  const handleClick = () => {
+    setDisabled(true)
+    console.log("jsj")
+    return props.onClick
+  }
+  
+
+
   return (
     <article className="movie">
       <img className="movie__thumbnail" src ={props.movie.Poster} alt="movie" />
@@ -9,7 +17,7 @@ export default function Movie(props) {
         <div className="movie__name">{props.movie.Title}</div>
         <div className="movie__artist">{props.movie.Year}</div>
       </div>
-      <button className="nominate-btn" onClick={props.onClick}>{props.message}</button>
+      <button className="nominate-btn" disabled={isDisabled} onClick={props.onClick}>{props.message}</button>
     </article>
   );
 }
