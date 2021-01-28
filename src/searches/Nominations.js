2fb1cd763049405875a7d6ff4ed13002bcec20fd
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-27 21:21:07 -0500

    modified the onclick props

--- src/searches/Nominations.js
+++ src/searches/Nominations.js
@@ -3,12 +3,11 @@ import Movie from './Movie'
 
 export default function Nominations(props) {
     const movies = props.movie
-  console.log(props, "nominations")
+  
   return (
     <section className="nominations">
       <h1>Your Nominations</h1>
-      {movies.map(movie => <Movie movie={movie} message="Remove" />)}
-      {console.log(props.movie, 'kkkkkk')}
+      {movies.map((movie, index) => <Movie key={index} movie={movie} message="Remove" onClick ={() => props.onClick(movie)}/>)}
     </section>
   );
 }
